885ccbe78d12255f310fac4f102a7b9df327a45b
Jonathan Vandionant <jonathan@MacBook-Pro.local>
2022-01-25 21:39:03 +0100

    Realized the form element

--- web/themes/custom/ef2_custom/js/theme/ready.js
+++ web/themes/custom/ef2_custom/js/theme/ready.js
@@ -1,3 +1,11 @@
 // Remove no-js class from body when Javascript is activated
 document.body.classList.remove('no-js');
 document.body.classList.add('js')
+
+var rad = document.querySelectorAll('.js-form-item-bezorginggegevens');
+for (var i = 0; i < rad.length; i++) {
+  rad[i].addEventListener('change', function() {
+    var radioChoice = document.querySelector('input[name="bezorginggegevens"]:checked').value;
+    document.getElementById("edit-bezorgdatum").setAttribute('value', radioChoice);
+  });
+}
