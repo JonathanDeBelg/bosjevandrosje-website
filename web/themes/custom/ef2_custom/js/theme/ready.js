938f2d8d0ca23bfbf58cd947d86c8fb05b3b51f1
Jonathan Vandionant <jonathan@wlan-222001.nbw.tue.nl>
2022-10-18 16:01:18 +0200

    WIP

--- web/themes/custom/ef2_custom/js/theme/ready.js
+++ web/themes/custom/ef2_custom/js/theme/ready.js
@@ -1,11 +1,3 @@
 // Remove no-js class from body when Javascript is activated
 document.body.classList.remove('no-js');
-document.body.classList.add('js')
-
-var rad = document.querySelectorAll('.js-form-item-bezorginggegevens');
-console.log(rad);
-for (var i = 0; i < rad.length; i++) {
-  rad[i].addEventListener('change', function() {
-
-  });
-}
+document.body.classList.add('js');
